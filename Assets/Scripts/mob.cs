7bb0a9d71df265080ab008dc3cffb3110c298598
Maria <mf.berguerand@etudiant.ecolemelies.fr>
2021-10-06 10:07:24 +0200

    MobQuiBouge

--- Assets/Scripts/mob.cs
+++ Assets/Scripts/mob.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mob : MonoBehaviour
+public class Mob : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;                                                           //s'occupe de la speed
+    private Transform target;                                                          //transforme la position du mob
+    
+
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();             //on declare aue la target est avec le tag Player sur le quel on reprend sa position
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime); //on change la position en 2D en faisant avancer le mob en lui faisant garder une speed constante
+                                                                                                               //moveDirection?
+
     }
 }
