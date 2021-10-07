ba3b676a4074b65bf88be83983b4bf7e76232e5b
Mathilde Degris <m.degris@etudiant.ecolemelies.fr>
2021-10-07 09:08:23 +0200

    Create Script Health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var damage = FindObjetcOfType<Mobs>();
+
+        damage.ReduireVie();
+    }
+}
