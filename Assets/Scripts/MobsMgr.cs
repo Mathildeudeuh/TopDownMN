0005d3a396439af039f9d3ab7df1f55a28775ac4
Maria <mf.berguerand@etudiant.ecolemelies.fr>
2021-10-11 09:18:55 +0200

    code

--- Assets/Scripts/MobsMgr.cs
+++ Assets/Scripts/MobsMgr.cs
@@ -13,6 +13,8 @@ public class MobsMgr : MonoBehaviour
     // Tableau pour les ennemis
     public GameObject[] ennemies;*/
 
+   // public float speed;                        //speed dans inspector
+
     public GamoObject ennemie;                  // Ennemis
 
     public GameObject[] ennemies;               //tableau dans inspector
@@ -21,9 +23,7 @@ public class MobsMgr : MonoBehaviour
 
     public float mobSpeed;                     // speed dans inspector
 
-    public float speed;                        //speed dans inspector
-    
-    public GameObject Target;                  // reference to player 
+    public GameObject target1;                  // reference to player 
 
     private Transform target;                  //nommer target    
 
@@ -37,8 +37,8 @@ public class MobsMgr : MonoBehaviour
         
         for (int i = 0; i < ennemies.Length; i++)             //i= 0 , i est plus petit que enemies; on ajoute i de 1 a chaque fois
         {
-            GameObject g = Instantiate(ennemies[i]);          //instantiate "ennemies" 
-            g.transform.position = GetRandSpawnPosition();    //
+            GameObject g = Instantiate(ennemies[i]);          //instantiate un clone de l "ennemies" 
+            g.transform.position = GetRandSpawnPosition();    //remplir la liste avec les prefabs
            
         }                                                                                                       
 
@@ -69,7 +69,7 @@ public class MobsMgr : MonoBehaviour
 
     Vector3 Spawn()
     {
-        int spawnIndex = Random.Range(0, 2);
+        int spawnIndex = Random.Range(0, 2);                        //Random.Range avec les 3 ennemis (0, 2)
         Instantiate(ennemies[spawnIndex]);                          //Instantiate les ennemis du tableau spawnIndex
         return transform.TransformPoint(Random.insideUnitCircle);   //mettre un random dans le cercle (insideUnitCircle)
     }
@@ -78,7 +78,7 @@ public class MobsMgr : MonoBehaviour
     void Update()
     {
 
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime); //on change la position en 2D en faisant avancer le mob en lui faisant garder une speed constante ainsi que la position du target
+        transform.position = Vector2.MoveTowards(transform.position, target.position, mobSpeed * Time.deltaTime); //on change la position en 2D en faisant avancer le mob en lui faisant garder une speed constante ainsi que la position du target
         Transform targetPosition = target.GetComponent<Transform>();
         for (int i = 0; i < spawnPoints.Count; i++)
         {
